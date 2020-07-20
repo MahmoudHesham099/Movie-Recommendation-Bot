ab52ea06a338f1ff89e5a7d8f66729c7243b1361
Sara <sarahak.676@gmail.com>
2020-07-20 18:15:10 +0200

    commit7

--- flaskblog.py
+++ flaskblog.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import csv
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 from ML import RecommendModel
-
+import bot
 app = Flask(__name__)
 
 # remove stop words like the/a
@@ -20,6 +20,25 @@ def index():
 def getMovies(title='Joker'):
     movieslist = RecommendModel.get_recommendations(title)
     return render_template('movies.html', movieslist=movieslist)
+
+
+@app.route("/get", methods = ["POST"])
+def chat():
+    if request.method == "POST":
+        rf = request.form
+        for key in rf.keys():
+            message = key
+        status, response = bot.chat(str(message))
+        data = {}
+
+        if status == 0:
+            data["reply"] = response
+            data["status"] = 'Sucess'
+            return data
+        else:
+            data["reply"] = response
+            data["status"] = 'Failed'
+            return data
 
 
 if __name__ == '__main__' :
