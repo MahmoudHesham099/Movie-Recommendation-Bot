0d20743f980cc8a00368c36b060dfe1283308cbe
Amiraalaaa <amiraalaaa1999@gmail.com>
2020-08-23 20:14:12 +0200

    utills Final added

--- utills.py
+++ utills.py
@@ -1,22 +1,38 @@
 import mysql.connector as mysql
-import datetime
 
 connection = mysql.connect(host="localhost", user="root",
-                           password="", db="flaskapp")
+                           password="", db="movierec")
+
+
+def saveAnswers(Answer,username):
+    cur = connection.cursor(dictionary=True)
+
+    query = "SELECT `user_id` FROM `userinfo` WHERE `username`= '{}'".format(username)
+    cur.execute(query)
+    userid = cur.fetchone()
+    userid = userid["user_id"]
+
+    query = "INSERT INTO `answers` (`user_id`,`input`) VALUES (%s,%s)"
+    cur.execute(query, (userid, Answer))
+    connection.commit()
+    cur.close()
+
+    return True
+
+
+def saveNewUser(Answer):
+    cur = connection.cursor(dictionary=True)
+    query = "INSERT INTO `userinfo` (`username`) VALUES (%s)"
+    cur.execute(query, (Answer,))
+    connection.commit()
+    cur.close()
+
+    return True
 
 def getUser(user):
     cur = connection.cursor(dictionary = True)
-    query = "SELECT * FROM `user_info` WHERE `username`= '{}'".format(user)
+    query = "SELECT * FROM `userinfo` WHERE `username`= '{}'".format(user)
     cur.execute(query)
     user = cur.fetchone()
     cur.close()
     return user
-
-def save(user_id):
-    cur = connection.cursor(dictionary=True)
-    query = "INSERT INTO `user_info` (user_id , date) VALUES(%s,%s)"
-    cur.execute(query, (user_id, datetime.datetime.now()))
-    connection.commit()
-    cur.close()
-    return True
-
